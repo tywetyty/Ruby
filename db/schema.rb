c15a86152e449547c483344f1184acdece3c57e4
tywetyty <tywetyty@gmail.com>
2015-10-09 10:44:35 +0800

    脚手架

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008071155) do
+ActiveRecord::Schema.define(version: 20151009011953) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -29,5 +29,12 @@ ActiveRecord::Schema.define(version: 20151008071155) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
